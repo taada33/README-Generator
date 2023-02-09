78dc350a680a8baa0265f643da3a7f9111577d62
Thomas Adamson <112778657+taada33@users.noreply.github.com>
2023-02-09 18:03:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// TODO: Include packages needed for this application
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -53,13 +52,13 @@ const questions = [
     }
 ];
 
-// TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, generateMarkdown(data), (err) =>
             err ? console.log(err) : console.log('Success!'));
 }
 
-// TODO: Create a function to initialize app
+
 function init() {
     inquirer
         .prompt(questions)
